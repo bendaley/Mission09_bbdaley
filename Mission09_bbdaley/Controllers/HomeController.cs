d6fa9f586832715841fe70035761e734d2a58821
bendaley <bbdaley21@gmail.com>
2023-02-28 23:03:33 -0700

    commit time baby

--- Mission09_bbdaley/Controllers/HomeController.cs
+++ Mission09_bbdaley/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_bbdaley.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,36 @@ namespace Mission09_bbdaley.Controllers
 {
     public class HomeController : Controller
     {
+        // private BookstoreContext context { get; set; }
+
+        // public HomeController(BookstoreContext temp) => context = temp;
+
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        /*
         public IActionResult Index()
         {
-            return View();
+            var home = context.Books.ToList();
+
+            return View(home);
+        }
+        */
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+
+            var home = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum -1) * pageSize)
+                .Take(pageSize);
+
+            return View(home);
         }
     }
 }
---
+++ Mission09_bbdaley/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_bbdaley.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = " page-blah")]
+    public class PaginationTagHelper : TagHelper
+    {
+        // dynamically create the page links
+    }
+}
