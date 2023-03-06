87e8bf0a8f05184594bec9374fbaa8ec4ed45e71
bendaley <bbdaley21@gmail.com>
2023-03-06 13:45:18 -0700

    bootstrap up to date

---
+++ Mission09_bbdaley/Components/TypesViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_bbdaley.Components
+{
+    public class TypesViewComponent : ViewComponent
+    {
+    }
+}
