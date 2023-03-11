78bfef21ed5f7b8c952ead272970215a9c009c3d
bendaley <bbdaley21@gmail.com>
2023-03-10 17:23:38 -0700

    video 11.1.3

--- Mission09_bbdaley/Models/SessionBasket.cs
+++ Mission09_bbdaley/Models/SessionBasket.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Mission09_bbdaley.Infrastructure;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,15 @@ namespace Mission09_bbdaley.Models
 {
     public class SessionBasket : Basket
     {
+        public static Basket GetBasket (IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            SessionBasket basket = session?.GetJson<SessionBasket>("Basket") ?? new SessionBasket();
+
+            return basket;
+        }
+        
         [JsonIgnore]
         public ISession Session { get; set; }
 
--- Mission09_bbdaley/Startup.cs
+++ Mission09_bbdaley/Startup.cs
@@ -40,6 +40,8 @@ namespace Mission09_bbdaley
             services.AddRazorPages();
             services.AddDistributedMemoryCache();
             services.AddSession();
+
+            services.AddScoped<Basket>(x => SessionBasket.GetBasket(x));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
