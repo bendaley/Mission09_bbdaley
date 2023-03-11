1c13c39a7cf3a6952eb7f0b5228f1fc42f52fda8
bendaley <bbdaley21@gmail.com>
2023-03-10 17:15:15 -0700

    video 11.1.2

---
+++ Mission09_bbdaley/Models/SessionBasket.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Mission09_bbdaley.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Mission09_bbdaley.Models
+{
+    public class SessionBasket : Basket
+    {
+        [JsonIgnore]
+        public ISession Session { get; set; }
+
+        public override void AddItem(Book bookbook, int qty)
+        {
+            base.AddItem(bookbook, qty);
+            Session.SetJson("Basket", this);
+        }
+
+        public override void RemoveItem(Book book)
+        {
+            base.RemoveItem(book);
+            Session.SetJson("Basket", this);
+        }
+
+        public override void ClearBasket()
+        {
+            base.ClearBasket();
+            Session.Remove("Basket");
+        }
+    }
+}
