f5d9c6f3f3ef64acca9150a346cfd12d0703ad6d
bendaley <bbdaley21@gmail.com>
2023-03-13 15:52:48 -0600

    video 11.2.4

---
+++ Mission09_bbdaley/Controllers/PurchaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_bbdaley.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_bbdaley.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View(new Purchase());
+        }
+    }
+}
